1005c1145c34d7eac5324b8ff982ca7df0c641a7
LDS196 <lihachevd87@gmail.com>
2023-05-02 22:21:01 +0400

    modified [id].tsx

--- pages/characters/[id].tsx
+++ pages/characters/[id].tsx
@@ -5,16 +5,18 @@ import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
 import {CharacterCard} from "../../components/Card/CharacterCard/CharacterCard";
 import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout";
 import {GetStaticPaths, GetStaticProps} from "next";
+import {useRouter} from "next/router";
+import styled from "styled-components";
 
 
-export const getStaticPaths:GetStaticPaths = async ()=>{
+export const getStaticPaths: GetStaticPaths = async () => {
     const {results} = await API.rickAndMorty.getCharacters()
-    const paths = results.map(character=>(
+    const paths = results.map(character => (
         {params: {id: String(character.id)}}
     ))
-    return{
+    return {
         paths,
-        fallback:false
+        fallback: 'blocking'
     }
 }
 export const getStaticProps: GetStaticProps = async ({params}) => {
@@ -36,13 +38,33 @@ type PropsType = {
     character: CharacterType
 }
 const Character = (props: PropsType) => {
+
     const {character} = props
+
+    const router = useRouter()
+
+    if (router.isFallback) return <h1>...Loading</h1>
+    const characterId = router.query.id
+    const goToCharacters = ()=>router.push('/characters')
+
 
     return (
         <PageWrapper>
+            <IdText>Id:{characterId}</IdText>
             <CharacterCard character={character}/>
+            <Button onClick={goToCharacters}>Go to Characters</Button>
         </PageWrapper>
     );
 };
 Character.getLayout = getLayout
 export default Character;
+
+const IdText = styled.div`
+  font-size: 40px;`
+const Button = styled.button`
+  width:  200px;
+  height: 50px;
+  background: aqua;
+  border: none;
+  border-radius: 5px;
+`
