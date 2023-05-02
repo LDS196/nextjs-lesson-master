513d2e925f7942bc14d927525db082edef25fc76
LDS196 <lihachevd87@gmail.com>
2023-05-02 20:25:19 +0400

    Initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,32 @@
+import type {AppProps} from 'next/app';
+import {ReactElement, ReactNode, useState} from 'react';
+import {NextPage} from 'next';
+import {QueryClient} from "@tanstack/query-core";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {Hydrate} from "@tanstack/react-query";
+import {useLoader} from "../assets/hooks/useLoader";
+import '../styles/nprogress.css'
+
+export type NextPageWithLayout<P = {}> = NextPage<P> & {
+    getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+
+export default function App({Component, pageProps}: AppPropsWithLayout) {
+    const [queryClient] = useState(() => new QueryClient())
+
+    const getLayout = Component.getLayout ?? ((page) => page);
+
+    useLoader()
+
+    return getLayout(
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydrateState}>
+                <Component {...pageProps} />
+            </Hydrate>
+        </QueryClientProvider>
+    );
+}
